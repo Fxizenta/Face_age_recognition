847b80835bb810131953962203305058a4868060
kai-soul <1476584706@qq.com>
2021-06-10 22:08:02 +0800

    updata:

--- age-gender-estimation/demo.py
+++ age-gender-estimation/demo.py
@@ -75,16 +75,17 @@ def yield_images_from_dir(image_dir):
 
 
 def analyze(img_dir):
-    #args = get_args()
-    #weight_file =
-    #global json_output
-    #global json_output, label, Dict
+    # args = get_args()
+    # weight_file =
+    # global json_output
+    # global json_output, label, Dict
     margin = 0.4
     image_dir = img_dir
 
-    #if not weight_file:
-    weight_file = get_file("EfficientNetB3_224_weights.11-3.44.hdf5", pretrained_model, cache_subdir="pretrained_models",
-                               file_hash=modhash, cache_dir=str(Path(__file__).resolve().parent))
+    # if not weight_file:
+    weight_file = get_file("EfficientNetB3_224_weights.11-3.44.hdf5", pretrained_model,
+                           cache_subdir="pretrained_models",
+                           file_hash=modhash, cache_dir=str(Path(__file__).resolve().parent))
 
     # for face detection
     detector = dlib.get_frontal_face_detector()
@@ -129,11 +130,11 @@ def analyze(img_dir):
                                         "M" if predicted_genders[i][0] < 0.5 else "F")
                 draw_label(img, (d.left(), d.top()), label)
 
-        #cv2.imshow("result", img)
+        # cv2.imshow("result", img)
         key = cv2.waitKey(-1) if image_dir else cv2.waitKey(30)
-       # cv2.imwrite("G:\\java\\result.jpg",img)
+        # cv2.imwrite("G:\\java\\result.jpg",img)
         Dict = {"age": label.split(",")[0], "gender": label.split(",")[1]}
-        json_output=json.dumps(Dict)
+        json_output = json.dumps(Dict)
         if key == 27:  # ESC
             break
     print("secces")
@@ -141,11 +142,11 @@ def analyze(img_dir):
     print(json_output)
     return json_output
 
+
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
-
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = os.getcwd()+'/uploads'
+app.config['UPLOAD_FOLDER'] = os.getcwd() + '/uploads'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 html = '''
@@ -167,36 +168,36 @@ html = '''
 
     var opacities = [0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,3,5,5,7,4,4,1,1,0,1,0,0,0,0,0,1,0,0,17,27,41,52,56,34,23,15,11,4,9,5,1,0,0,0,0,0,0,1,45,63,57,45,78,66,52,41,34,37,23,20,0,1,0,0,0,0,1,43,62,66,64,67,115,112,114,56,58,47,33,18,12,10,0,0,0,0,39,50,63,76,87,107,105,112,128,104,69,64,29,18,21,15,0,0,0,7,42,52,85,91,103,126,153,128,124,82,57,52,52,24,1,0,0,0,2,17,41,67,84,100,122,136,159,127,78,69,60,50,47,25,7,1,0,0,0,34,33,66,82,113,138,149,168,175,82,142,133,70,62,41,25,6,0,0,0,18,39,55,113,111,137,141,139,141,128,102,130,90,96,65,37,0,0,0,2,15,27,71,104,129,129,158,140,154,146,150,131,92,100,67,26,3,0,0,0,0,46,73,104,124,145,135,122,107,120,122,101,98,96,35,38,7,2,0,0,0,50,58,91,124,127,139,118,121,177,156,88,90,88,28,43,3,0,0,0,0,30,62,68,91,83,117,89,139,139,99,105,77,32,1,1,0,0,0,0,0,16,21,8,45,101,125,118,87,110,86,64,39,0,0,0,0,0,0,0,0,0,1,28,79,79,117,122,88,84,54,46,11,0,0,0,0,0,0,0,0,0,1,0,6,55,61,68,71,30,16,0,0,0,0,0,0,0,0,0,0,0,0,0,0,14,23,25,20,12,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,12,9,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4,2,2,0,0,0,0,0,0,0,0]
     var smokeSpriteSize = 20
-    
+
     var polyfillAnimFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
         window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
-    
+
     function floatInRange(start, end){
         return start + Math.random()*(end - start)
     }
-    
+
     function makeSmokeSprite(color){
         color = [100 ,149, 237]||color  
         var smokeSprite = document.createElement('canvas'),
             ctx = smokeSprite.getContext('2d'),
             data = ctx.createImageData(smokeSpriteSize, smokeSpriteSize),
             d = data.data
-    
+
         for(var i=0;i<d.length;i+=4){
             d[i]=color[0]
             d[i+1]=color[1]
             d[i+2]=color[2]
             d[i+3]=opacities[i / 4]
         }
-    
+
         smokeSprite.width = smokeSpriteSize
         smokeSprite.height = smokeSpriteSize
-    
+
         ctx.putImageData(data,0,0)
-    
+
         return smokeSprite
     }
-    
+
     function createParticle(x,y,options){
         options = options || {}
         var lifetime = options.lifetime || 4000
@@ -216,7 +217,7 @@ html = '''
         particle.vy = particle.startvy
         return particle
     }
-    
+
     function updateParticle(particle, deltatime){
         particle.x += particle.vx * deltatime
         particle.y += particle.vy * deltatime
@@ -225,7 +226,7 @@ html = '''
         particle.age+=deltatime
         particle.scale=frac*particle.finalScale
     }
-    
+
     function drawParticle(particle, smokeParticleImage, context){
         context.globalAlpha = (1-Math.abs(1-2*particle.age/particle.lifetime))/8
         var off = particle.scale*smokeSpriteSize/2
@@ -236,39 +237,39 @@ html = '''
         context.drawImage(smokeParticleImage, xmin, ymin, xmax-xmin, ymax-ymin)
         // console.log(smokeParticleImage, xmin, ymin, xmax-xmin, ymax-ymin)
     }
-    
-    
+
+
     return function SmokeMachine(context, color){
         var smokeParticleImage = makeSmokeSprite(color),
             particles = [],
             preDrawCallback=function(){}
-    
+
         function updateAndDrawParticles(deltatime){
             context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-    
+
             particles.forEach(function(p){ updateParticle(p, deltatime) })
             particles = particles.filter(function(p){ return p.age < p.lifetime })
-    
+
             preDrawCallback(deltatime, particles)
             particles.forEach(function(p){ drawParticle(p, smokeParticleImage, context) })
         }
-    
+
         var running = false, lastframe = performance.now()
         function frame(time){
             if(!running) return
             var dt = time-lastframe
             lastframe = time;
-    
+
             updateAndDrawParticles(dt)
             polyfillAnimFrame(frame)
         }
-    
+
         function addParticles(x,y,numParticles,options){
             numParticles = numParticles || 10
             if(numParticles < 1) return Math.random() <= numParticles && particles.push(createParticle(x,y,options));
             for (var i = 0; i < numParticles; i++) particles.push(createParticle(x,y,options))
         }
-    
+
         return {
             step: function step(dt){
                 dt = dt || 16
@@ -340,6 +341,38 @@ html = '''
           left: 825px;
           z-index: 9998;
      }
+     .image {
+         width:200px;
+         height:200px;
+         position:absolute;
+         top:225px;
+         left:672px;
+         z-index:9998;
+     }
+     .age {
+         font-family:"Trebuchet MS", Arial, Helvetica, sans-serif;
+         font-size: 53px;
+         color: #FF8C00;
+         position: absolute;
+         top: 21px;
+         left: 646px;
+    }
+    .gender {
+        font-family:"Trebuchet MS", Arial, Helvetica, sans-serif;
+         font-size: 53px;
+         color: #FF8C00;
+         position: absolute;
+         top: 80px;
+         left: 646px;
+    }
+    .frame {
+        width: 278px;
+        height: 467px;
+        position: absolute;
+        top: 94px;
+        left: 633px;
+        z-index: 999;
+    }
 </style>  
     <title>Upload File</title>
     <form method=post enctype=multipart/form-data>
@@ -348,7 +381,7 @@ html = '''
     </form>
     <img src="https://picgo-1304285457.cos.ap-guangzhou.myqcloud.com/images/754ac3d0b2d29dfef80497e106537de.png" class="file"></img>
     <img src="https://picgo-1304285457.cos.ap-guangzhou.myqcloud.com/images/76eb158d7b7c27cdc9872287bf0093c.png" class="upload_ico"></img>
-
+    <img src="https://picgo-1304285457.cos.ap-guangzhou.myqcloud.com/images/%E5%8D%83%E5%BA%93%E7%BD%91_%E8%93%9D%E8%89%B2%E7%A7%91%E6%8A%80%E5%87%A0%E4%BD%95%E8%BE%B9%E6%A1%86_%E5%85%83%E7%B4%A0%E7%BC%96%E5%8F%B712557974.png" class=frame></img>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
 <style type="text/css">
@@ -452,16 +485,16 @@ def upload_file():
     if request.method == 'POST':
         file = request.files['file']
         if file and allowed_file(file.filename):
-            #filename = secure_filename(file.filename)
-            filename=file.filename
+            # filename = secure_filename(file.filename)
+            filename = file.filename
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            file_url = '.'+url_for('uploaded_file', filename=filename)
-            #print("."+file_url)
+            file_url = '.' + url_for('uploaded_file', filename=filename)
+            # print("."+file_url)
             js = json.loads(analyze(file_url))
             age = js["age"]
             gender = js["gender"]
             html + '<div>' + age + '</div>'
-            return html + '<br><img src=' + file_url + '>' + '<div>' + file_url + '</div>' + '<div>' + age + '</div>'
+            return html + '<img class=image src=' + file_url + '>' + '<p class=age> Age:' + age + '</p>' + '<p class=gender> Gender:' + gender + '</p>'
     return html
 
 
